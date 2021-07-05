7bbfbb3daf69de5d70b53ad47655e920978370af
Luan van Pletsen <luanvanpletsen@gmail.com>
2021-07-05 11:08:36 +0100

    test tokenize()

--- tests.py
+++ tests.py
@@ -1,19 +1,28 @@
 import unittest
+import random
 from questions import *
 
 class TestQuestions(unittest.TestCase):
 
+    data = {}
+
     def setUp(self):
-        pass
+        directory = './corpus/'
+        self.data = load_files(directory)
 
     def test_load_files(self):
-        directory = './corpus/'
-        data = load_files(directory)
         # test keys of data dictiorary
-        self.assertEqual(set(data.keys()), {'artificial_intelligence.txt','machine_learning.txt', 'natural_language_processing.txt','neural_network.txt', 'probability.txt','python.txt'})
+        self.assertEqual(set(self.data.keys()), {'artificial_intelligence.txt','machine_learning.txt', 'natural_language_processing.txt','neural_network.txt', 'probability.txt','python.txt'})
         # test strings in dictionary
-        self.assertIsInstance(data['natural_language_processing.txt'], str)
-        self.assertGreater(len(data['natural_language_processing.txt']), 0)
+        self.assertIsInstance(self.data['natural_language_processing.txt'], str)
+        self.assertGreater(len(self.data['natural_language_processing.txt']), 0)
+
+    def test_tokenize(self):
+        tokens = tokenize(self.data['natural_language_processing.txt'])
+        samples = random.sample(tokens, 5)
+        for sample in samples:
+            print(sample)
+            self.assertTrue(sample.islower())
     
 if __name__ == '__main__':
     unittest.main()
